1a0be247fb461f0d43f3472bd5562f45a983fabe
Bianca Fernandes Nascimento <90284053+BFN100@users.noreply.github.com>
2022-11-29 21:01:46 -0500

    Create eLangagesProg.cs

---
+++ eLangagesProg.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Societe_Developpeurs
+{
+    public enum eLangagesProg 
+    {
+        Java,
+        Web,
+        Csharp,
+        CplusPlus,
+        Python
+    }
+}
