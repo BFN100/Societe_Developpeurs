7d7304c5a3d52292e32441cb2e53980ce4b24648
Bianca Fernandes Nascimento <90284053+BFN100@users.noreply.github.com>
2022-11-29 21:02:08 -0500

    Create iDeveloppeurs.cs

---
+++ iDeveloppeurs.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Societe_Developpeurs
+{
+    public interface iDeveloppeurs
+    {
+        String getNom();
+        String getCompetence();
+    }
+}
